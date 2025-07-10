f458ddff931f547bfa748718667f4f640476ff3d
TheOne-akc <Alokkc1@outlook.com>
2025-07-10 12:16:30 +0000

    04 condition

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,30 @@
+//if
+// const isUserLoggedIn= true
+// const temp = 41
+
+// if (temp===40) {
+//     console.log("temp is below 50");
+// }else
+// {
+//     console.log("temp is above 50");
+// }
+
+// const score =100
+// if (score > 99) {
+//     let Power='Fly'
+//     console.log(`User Power : ${Power}`);
+    
+// }
+// console.log(`User Power:${Power}`);
+
+//if (balance>500) console.log("test"),console.log("test2");
+const balance =1000
+if (balance < 500) {
+    console.log("less than 500");
+    } 
+else if (balance < 750) {
+    console.log("less than 750");
+}else{
+    console.log("less than 1200");
+}
+    
